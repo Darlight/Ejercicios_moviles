6e95f7d77fa4cc89750891f51af76999c07af816
Darlight <andres.perdomo09@gmail.com>
2019-01-19 18:30:40 -0600

    Lab_creado

---
+++ Lab#2/Restaurant/app/src/main/java/com/example/marioandres/restaurant/Front_Page.kt
@@ -0,0 +1,12 @@
+package com.example.marioandres.restaurant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Front_Page : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front__page)
+    }
+}
---
+++ Lab#2/Restaurant/app/src/main/java/com/example/marioandres/restaurant/Petition.kt
@@ -0,0 +1,12 @@
+package com.example.marioandres.restaurant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Petition : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_petition)
+    }
+}
